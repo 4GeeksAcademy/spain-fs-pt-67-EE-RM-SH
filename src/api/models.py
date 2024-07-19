1c43a6e56b6304cdd81d23f8a8f27c584c6d3138
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-07-19 19:27:52 +0000

    cambios

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id= db.Column(db.Integer, primary_key=True)
+    role  = db.Column(db.String(120), unique=True, nullable=False)
+    name  = db.Column(db.String(120), unique=True, nullable=False)
+    lastname  = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +19,35 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "role": self.role,
             "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+
             # do not serialize the password, its a security breach
         }
+
+class Lesson(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    url_video= db.Column(db.String(120), unique=True, nullable=False)
+    category= db.Column(db.String(120), unique=True, nullable=False)
+    title= db.Column(db.String(120), unique=True, nullable=False)
+    description= db.Column(db.String(120), unique=True, nullable=False)
+    author= db.Column(db.String(120), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "url": self.url,
+            "category": self.category,
+            "title": self.title,
+            "description": self.description,
+            "author": self.author
+            # do not serialize the password, its a security breach
+        }
+    
+
+
+
+
+  
