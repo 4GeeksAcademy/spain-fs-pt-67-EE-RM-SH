6a7aafd590b573f5d8c073dd3cab5dae64cec096
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-07-20 18:24:53 +0000

    cambios de las tablas

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Lesson, Courses, Order, Order_item
+from .models import db, User, Lesson, Courses, Orders, Order_item
 from flask_admin.contrib.sqla import ModelView
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -13,7 +13,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Lesson, db.session))
     admin.add_view(ModelView(Courses, db.session))
-    admin.add_view(ModelView(Order, db.session))
+    admin.add_view(ModelView(Orders, db.session))
     admin.add_view(ModelView(Order_item, db.session))
 
     # You can duplicate that line to add mew models
--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -11,38 +13,19 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
         return {
-
             "id": self.id,
             "role": self.role,
             "email": self.email,
             "name": self.name,
             "lastname": self.lastname,
             # do not serialize the password, its a security breach
-        }
-
-class Lesson(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url_video = db.Column(db.String(120), unique=True, nullable=False)
-    category = db.Column(db.String(120), unique=True, nullable=False)
-    title = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(120), unique=True, nullable=False)
-    author = db.Column(db.String(120), unique=True, nullable=False)
-
-    def serialize(self):
-        return {
-
-            "id": self.id,
-            "url_video": self.url_video,
-            "category": self.category,
-            "title": self.title,
-            "description": self.description,
-            "author": self.author
         }
     
 class Courses(db.Model):
@@ -52,8 +35,8 @@ class Courses(db.Model):
     price = db.Column(db.String(120), unique=True, nullable=False)
 
     def serialize(self):
+    
         return {
-
             "id": self.id,
             "name": self.name,
             "description": self.description,
@@ -61,42 +44,65 @@ class Courses(db.Model):
         }
 
 
-class Order(db.Model):
+class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(120), unique=True, nullable=False)
+    url_video = db.Column(db.String(120), unique=True, nullable=False)
+    category = db.Column(db.String(120), unique=True, nullable=False)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+    author = db.Column(db.String(120), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey(Courses.id), nullable=False)
+   
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "url_video": self.url_video,
+            "category": self.category,
+            "title": self.title,
+            "description": self.description,
+            "author": self.author,
+            "user_id": self.user_id,
+            "course_id": self.course_id
+        }
+    
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     methods_payment = db.Column(db.String(120), unique=True, nullable=False)
     payment_date = db.Column(db.String(120), unique=True, nullable=False)
     total = db.Column(db.String(120), unique=True, nullable=False)
     status = db.Column(db.String(120), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+
 
     def serialize(self):
         return {
-
             "id": self.id,
             "user_id": self.user_id,
             "methods_payment": self.methods_payment,
             "payment_date": self.payment_date,
             "total": self.total,
-            "status": self.status
+            "status": self.status,
+            "user_id": self.user_id
         }
     
 class Order_item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    quantity = db.Column(db.String(120), unique=True, nullable=False)
-    course_id = db.Column(db.String(120), unique=True, nullable=False)
-    order_id = db.Column(db.String(120), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey(Courses.id), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey(Orders.id), nullable=False)
 
     def serialize(self):
         return {
-
             "id": self.id,
             "quantity": self.quantity,
             "course_id": self.course_id,
             "order_id": self.order_id,
         }
 
-    def to_dict(self):
-        return {}
 
 
 
