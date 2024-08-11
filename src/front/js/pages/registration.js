ec4f529188571de4cb98081c6238cb03c96691a4
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-08-11 19:44:55 +0000

    rama login

--- src/front/js/pages/registration.js
+++ src/front/js/pages/registration.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
+
 import { Context } from "../store/appContext";
 
 export const Registration = () => {
