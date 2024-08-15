50ba8e2f3ed7832cc6b9689eaa373674d49c60cf
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-08-15 19:29:04 +0000

    cambios de importación de useNavigate

--- src/front/js/pages/registration.js
+++ src/front/js/pages/registration.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link,  useNavigate } from "react-router-dom";
 
 
 import { Context } from "../store/appContext";
 
 export const Registration = () => {
     const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
 
     // const {actions} = useContext(Context)
     const [email, setEmail] = useState('');
