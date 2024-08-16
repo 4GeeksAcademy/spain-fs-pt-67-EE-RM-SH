48912e69fb62f261f0381d365baa490daefee2af
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-08-16 19:08:08 +0000

    cambio en el registration

--- src/front/js/pages/registration.js
+++ src/front/js/pages/registration.js
@@ -17,69 +17,69 @@ export const Registration = () => {
     const handleRegistrer = async (e) => {
         e.preventDefault()
         const createUser = await actions.createUser(email, password, name, lastname, role)
-        if (createUser.access_token) {
+        if (createUser) {
             navigate("/"); // Redirige a la pagina student si el inicio de sesión es exitoso
         } else {
             alert("Login failed. Please check your credentials.");
 
-            if (!!createUser.access_token) {
+            if (createUser) {
                 navigate("/student");
             }
         }
-
-
-        return (
-            <div className="login-container">
-                <form onSubmit={handleRegistrer}>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="text"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="lastname">Lastname:</label>
-                        <input
-                            type="text"
-                            id="lastname"
-                            value={lastname}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="role">Role:</label>
-                        <input
-                            type="text"
-                            id="role"
-                            value={role}
-                            onChange={(e) => setRole(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit">Registrarse</button>
-                </form>
-
-            </div>
-        );
     };
+
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleRegistrer}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="lastname">Lastname:</label>
+                    <input
+                        type="text"
+                        id="lastname"
+                        value={lastname}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="role">Role:</label>
+                    <input
+                        type="text"
+                        id="role"
+                        value={role}
+                        onChange={(e) => setRole(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Registrarse</button>
+            </form>
+
+        </div>
+    );
 }
